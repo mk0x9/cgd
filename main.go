ad1112f5d61c7de46c8508b8f3ef55ca9c5b3142
Michael Kuryshev <me@mk9.name>
2015-04-09 00:04:53 +0300

    More versatility in handling environment variables and http headers.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strings"
 
 	"net"
 	"net/http"
@@ -14,8 +15,11 @@ import (
 var cmd = flag.String("c", "", "CGI program to run")
 var pwd = flag.String("w", "", "Working dir for CGI")
 var serveFcgi = flag.Bool("f", false, "Run as a FCGI 'server' instead of HTTP")
-var debug = flag.Bool("debug", false, "Print debug msgs to stderr.")
+var debug = flag.Bool("debug", false, "Print debug msgs to stderr")
 var address = flag.String("a", ":3333", "Listen address")
+var envVars = flag.String("e", "", "Comma-separated list of environment variables to preserve")
+
+var passtroughHeaders = []string{"AUTH_TYPE", "REMOTE_USER"} // set of headers to passthrough from http backend
 
 func main() {
 	flag.Usage = usage
@@ -33,23 +37,45 @@ func main() {
 
 	os.Setenv("PATH", os.Getenv("PATH")+":.")
 
-	h := &cgi.Handler{
-		Path:       c,
-		Root:       "/",
-		Dir:        *pwd,
-		InheritEnv: []string{"PATH", "PLAN9"},
+	envList := []string{"PATH", "PLAN9"}
+	for _, envVar := range strings.Split(*envVars, ",") {
+		envList = append(envList, envVar)
 	}
 
 	var err error
 	if *serveFcgi {
 		if l, err := net.Listen("tcp", *address); err == nil {
 			log.Println("Starting FastCGI daemon listening on", *address)
-			err = fcgi.Serve(l, h)
+			err = fcgi.Serve(l, &cgi.Handler{
+				Path:       c,
+				Root:       "/",
+				Dir:        *pwd,
+				InheritEnv: envList,
+			})
 		}
 
 	} else {
 		log.Println("Starting HTTP server listening on", *address)
-		err = http.ListenAndServe(*address, h)
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			var acc = []string{}
+			for header := range r.Header {
+				if v := r.Header.Get(header); v != "" {
+					for _, pHeader := range passtroughHeaders {
+						if strings.ToLower(header) == strings.ToLower(pHeader) {
+							acc = append(acc, pHeader+"="+v)
+						}
+					}
+				}
+			}
+			(&cgi.Handler{
+				Path:       c,
+				Root:       "/",
+				Dir:        *pwd,
+				InheritEnv: envList,
+				Env:        acc,
+			}).ServeHTTP(w, r)
+		})
+		err = http.ListenAndServe(*address, nil)
 	}
 
 	if err != nil {
@@ -58,7 +84,7 @@ func main() {
 }
 
 func usage() {
-	os.Stderr.WriteString("usage: cgd [-s] -c prog [-w wdir] [-a addr]\n")
+	os.Stderr.WriteString("usage: cgd -c prog [-w wdir] [-a addr] [-e FOO,BAR]\n")
 	flag.PrintDefaults()
 	os.Exit(2)
 }
